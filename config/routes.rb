a19321e991f75a2c9d1a9d75f31fa37e98e18ebe
Alexandre Lemaitre <alexandre.lemaitre2@gmail.com>
2020-06-02 19:31:09 +0200

    added controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'favorites/index'
+  get 'favorites/show'
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'bookings/index'
+  get 'bookings/create'
+  get 'bookings/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
