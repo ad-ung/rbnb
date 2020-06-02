6a9c8481bb33fadc9be4e6ab925232c2948054ed
Alexandre Lemaitre <alexandre.lemaitre2@gmail.com>
2020-06-02 19:47:25 +0200

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources: villas, only: [:index, :show]
-  resources: bookings, only: [:index, :show, :create]
-  resources: favorites, only: [:index, :show]
-  resources: reviews, only: [:new, :create]
+  resources :villas, only: [:index, :show]
+  resources :bookings, only: [:index, :show, :create]
+  resources :favorites, only: [:index, :show]
+  resources :reviews, only: [:new, :create]
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
