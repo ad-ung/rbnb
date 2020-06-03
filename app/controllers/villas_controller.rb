3b908f7c675af0cd818a10bf22f2cb00bd1b2721
Adeline Ung <ungadeline93@hotmail.com>
2020-06-03 15:17:14 +0200

    created simple-form

--- app/controllers/villas_controller.rb
+++ app/controllers/villas_controller.rb
@@ -1,5 +1,9 @@
 class VillasController < ApplicationController
   def index
+    @city = params[:city]
+    if @city.present?
+      @villas = Villa.where(city: params[:city])
+    end
   end
 
   def show
