9e864fe5b246efaeea862fb59936af86e3ab71de
Bernadette d'Almeida <b.dalmeida@hotmail.fr>
2020-06-04 23:03:46 +0200

    create method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,8 +3,21 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @villa = Villa.find(params[:id])
+    @booking = Booking.new(booking_params)
+    @booking.villa = @villa
+    @booking.user = current.user
+    @booking.save
   end
 
   def show
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
+
+
