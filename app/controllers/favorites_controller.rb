17ccb82c3a1f8058ccaaca1073f863742cd9747a
Adeline Ung <ungadeline93@hotmail.com>
2020-06-09 15:59:14 +0200

    added create in favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,4 +3,12 @@ class FavoritesController < ApplicationController
   def index
     @favorites = Favorite.all
   end
+
+  def create
+    @villa = Villa.find(params[:villa_id])
+    @favorite = Favorite.new
+    @favorite.villa = @villa
+    @booking.user = current_user
+    @booking.save
+  end
 end
