adae90aed8b65da30e833d4942977c588be41727
Adeline Ung <ungadeline93@hotmail.com>
2020-06-09 17:05:35 +0200

    dates from searchbar reported into villa show

--- app/javascript/components/calendar.js
+++ app/javascript/components/calendar.js
@@ -1,6 +1,12 @@
 import flatpickr from "flatpickr";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
+const convertDate = (inputFormat) => {
+  function pad(s) { return (s < 10) ? '0' + s : s; }
+  var d = new Date(inputFormat)
+  return [pad(d.getDate()), pad(d.getMonth()+1), d.getFullYear()].join('/')
+}
+
 const toggleDateInputs = function() {
   const startDateInput = document.getElementById('booking_start_date');
   const endDateInput = document.getElementById('booking_end_date');
@@ -11,15 +17,21 @@ const toggleDateInputs = function() {
     flatpickr(startDateInput, {
       minDate: 'today',
       dateFormat: 'd/m/Y',
-      plugins: [new rangePlugin({ input: endDateInput})]
+      plugins: [new rangePlugin({ input: endDateInput})],
+      onClose: function(selectedDates) {
+        sessionStorage.setItem("startDate", convertDate(selectedDates[0]));
+        sessionStorage.setItem("endDate", convertDate(selectedDates[1]));
+      }
     });
   }
 
   if (startDateInput2 && endDateInput2) {
     const unvailableDates = JSON.parse(document.querySelector('.widget-content').dataset.unavailable);
+    debugger;
     flatpickr(startDateInput2, {
       minDate: 'today',
       dateFormat: 'd/m/Y',
+      defaultDate: [sessionStorage.startDate,sessionStorage.endDate],
       disable: unvailableDates,
       plugins: [new rangePlugin({ input: endDateInput2})]
     });
