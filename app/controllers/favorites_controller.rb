f77b432b921aad2a949a91bf8591f90c616196fd
Bernadette d'Almeida <b.dalmeida@hotmail.fr>
2020-06-10 16:10:56 +0200

    add delete a revoir

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,22 +1,38 @@
 class FavoritesController < ApplicationController
-
+  before_action :set_villa, only: [:create]
   def index
     @favorites = Favorite.all
   end
 
-  def show
-    @favorite = Favorite.find(params[:villa_id])
-    #@villa = @favorite.villa
-  end
-
   def create
     @villa = Villa.find(params[:villa_id])
-    @favoris = Favorite.new
-    @favoris.villa = @villa
-    @favoris.user = current_user
-    @favoris.save
+    @favorite = Favorite.new
+    @favorite.villa = @villa
+    @favorite.user = current_user
+    @in_favorite_list = true
+    @favorite.save
   end
 
+
+  def update
+    @all_favorites = [] << @favorites
+    if @all_favorites.include?(@villa)
+      @all_favorites.delete(@villa)
+      @in_favorite_list = false
+    else
+      @favorite = Favorite.new(favorite_params)
+      @favorite.villa = @villa
+      @favorite.user = current_user
+      @favorite.save
+      @in_favorite_list = true
+    end
+
+  end
+
+  def destroy
+    @favorite.destroy
+    @in_favorite_list = false
+  end
 
   private
 
