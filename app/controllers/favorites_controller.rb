23eeac544078efe996ed4b70b4b8aac3b60a6d76
Bernadette d'Almeida <b.dalmeida@hotmail.fr>
2020-06-13 10:54:16 +0200

    pb icones

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,6 +10,7 @@ class FavoritesController < ApplicationController
     @favorite.villa = @villa
     @favorite.user = current_user
     @favorite.save
+    redirect_to villa_path(@villa)
   end
 
 
@@ -18,6 +19,7 @@ class FavoritesController < ApplicationController
     @villa = Villa.find(params[:id])
     @favorite = Favorite.where(user_id: current_user.id, villa_id: @villa.id)
     Favorite.destroy(@favorite.first.id)
+    redirect_to villa_path(@villa)
   end
 
 
