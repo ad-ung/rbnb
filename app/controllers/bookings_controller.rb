8cc39197f26160c661887c505d205361beeef6f1
Alexandre Lemaitre <alexandre.lemaitre2@gmail.com>
2020-06-13 15:49:16 +0200

    change booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,7 +15,7 @@ class BookingsController < ApplicationController
   end
 
   def show
-    # @booking = Booking.find(params[:id])
+    @booking = Booking.find(params[:id])
     @markers = [ {
       lat: @booking.villa.latitude,
       lng: @booking.villa.longitude
